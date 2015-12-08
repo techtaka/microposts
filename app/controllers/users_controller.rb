746f28e813cf9b098d27fb40add6716a319578a8
techtaka <techaca@ht777.halfmoon.jp>
2015-12-08 13:01:52 +0000

    kadai_Profile renewal

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,45 @@
 class UsersController < ApplicationController
- def show
-  @user = User.find(params[:id])
- end
+
+ before_action :set_user, only: [:show, :edit, :update]
+
+  def show
+  end
   
- def new
-  @user = User.new
- end
+  def new
+    @user = User.new
+  end
 
- def create
-  @user = User.new(user_params)
-   if @user.save
-    flash[:success] = "Welcome to the Sample App!"
-    redirect_to @user
-   else
-    render 'new'
-   end
- end
+  def create
+    @user = User.new(user_params)
+      if @user.save
+        flash[:success] = "Welcome to the Sample App!"
+        redirect_to @user
+      else
+        render 'new'
+      end
+  end
 
- private
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:success] = "The contents were renewed."
+      redirect_to @user
+    else
+      render 'edit'
+    end  
+  end
+
+private
 
   def user_params
-   params.require(:user).permit(:name, :email,
+    params.require(:user).permit(:name, :email, :area,
                                 :password, :password_confirmation)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 
 end
