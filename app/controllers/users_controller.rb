8f9bcd917944fb8ce9019b94244b82f9f2ef5d3e
techtaka <techaca@ht777.halfmoon.jp>
2015-12-10 11:49:50 +0000

    user-profile3-2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,14 @@
 class UsersController < ApplicationController
 
   before_action :set_user, only: [:show, :edit, :update]
-  
+
   def show
   end
-  
+
   def new
     @user = User.new
   end
-  
+
   def create
     @user = User.new(user_params)
       if @user.save
@@ -18,10 +18,10 @@ class UsersController < ApplicationController
         render 'new'
       end
   end
-  
+
   def edit
   end
-  
+
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
@@ -31,14 +31,14 @@ class UsersController < ApplicationController
       render 'edit'
     end  
   end
-  
+
   private
-  
+
   def user_params
     params.require(:user).permit(:name, :email, :area,
                                 :password, :password_confirmation)
   end
-  
+
   def set_user
     @user = User.find(params[:id])
   end
