4f8560f459bedc4649d07c5612fd0f927adfbb26
techtaka <techaca@ht777.halfmoon.jp>
2015-12-13 12:54:49 +0000

    add followings/followers

--- config/application.rb
+++ config/application.rb
@@ -4,10 +4,12 @@ require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
+
 Bundler.require(*Rails.groups)
 
 module Microposts
- class Application < Rails::Application
+  class Application < Rails::Application
+
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
   # -- all .rb files in that directory are automatically loaded.
@@ -23,12 +25,13 @@ module Microposts
   # Do not swallow errors in after_commit/after_rollback callbacks.
   # config.active_record.raise_in_transactional_callbacks = true
     
-  config.generators do |g|
-   g.stylesheets false
-   g.javascripts false
-   g.helper false
-   g.test_framework false
-    end
-    
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.test_framework false
+      end
+  
+    config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
