96fd685cf44f48f8827840482ba15843b33a081d
techtaka <techaca@ht777.halfmoon.jp>
2015-12-17 14:15:08 +0000

    followings/followers3

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +6,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# User.create!(name: 'hashimoto', email: 'web@dyo.jp', password: 'password', password_confirmation: 'password', area: 'yokohama' )
+
+# [100..110].each do |i|
+#     User.create(name: "user#{i}", email: "example#{i}@example.com", password: 'password', password_confirmation: 'password')
+# end
